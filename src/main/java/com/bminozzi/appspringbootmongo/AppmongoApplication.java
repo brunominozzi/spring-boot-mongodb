ce97573a7630b6f97c06abe7cec6c35383282437
brunominozzi <bminozzi@gmail.com>
2019-10-24 02:21:38 -0300

    Primeiro commit - Projeto Criado

---
+++ src/main/java/com/bminozzi/appspringbootmongo/AppmongoApplication.java
@@ -0,0 +1,13 @@
+package com.bminozzi.appspringbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bminozzi/appspringbootmongo/AppmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bminozzi.appspringbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
