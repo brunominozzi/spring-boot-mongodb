faa6c668cc9a67248cf3007725dcf0075ebb189c
brunominozzi <bminozzi@gmail.com>
2019-10-31 16:10:12 -0300

    Obtendo usuario por id

--- src/main/java/com/bminozzi/appspringbootmongo/services/UserService.java
+++ src/main/java/com/bminozzi/appspringbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.bminozzi.appspringbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bminozzi.appspringbootmongo.domain.User;
 import com.bminozzi.appspringbootmongo.repository.UserRepository;
+import com.bminozzi.appspringbootmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!")); 
 	}
 
 }
