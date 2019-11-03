c839111645608355fe5b509f70ef6ee34640609c
brunominozzi <bminozzi@gmail.com>
2019-11-03 01:09:30 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/bminozzi/appspringbootmongo/domain/Post.java
+++ src/main/java/com/bminozzi/appspringbootmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.bminozzi.appspringbootmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.bminozzi.appspringbootmongo.dto.AuthorDTO;
+import com.bminozzi.appspringbootmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	public String title;
 	public String body;
 	public AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post(){
 		
@@ -71,6 +76,39 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 
